9c0ef0b70bd2e749e6c24cf8eaac597c736f7e93
Bhawna198 <122960027+Bhawna198@users.noreply.github.com>
2025-08-02 18:47:21 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,6 +21,7 @@ function App() {
       <Router>
         <div className="App">
           <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route
